8ffc83dd3a6764fb4c4f32e8beb87380b98010a3
Stanislav <staskornienko01@gmail.com>
2024-11-25 19:48:04 +0100

    Initial commit

---
+++ console/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console
+{
+
+    class NewClass
+    {
+        static void Main(string[] args)
+        {
+
+            int userX = 5; int userY = 7;
+            char[,] map =
+                   {
+               {'#','#','#','#','#','#','#','#','#','#','#','#','#','#' },
+               {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#' },
+               {'#',' ',' ',' ',' ',' ','X',' ',' ',' ',' ',' ',' ','#' },
+               {'#',' ','X',' ',' ',' ',' ',' ',' ',' ','X',' ',' ','#' },
+               {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#' },
+               {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#' },
+               {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#' },
+               {'#',' ',' ',' ',' ','#','#','#','#','#',' ',' ',' ','#' },
+               {'#',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ','#' },
+               {'#',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ','#' },
+               {'#',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ',' ','#' },
+               {'#','#','#','#','#','#','#','#','#','#','#','#','#','#' }
+
+            };
+            while (true)
+            {
+                
+                Console.Clear();
+
+
+                {
+                    Console.CursorVisible = false;
+                    
+                    for (int i = 0; i < map.GetLength(0); i++)
+                    {
+
+                        for (int j = 0; j < map.GetLength(1); j++)
+                        {
+                            Console.Write(map[i, j]);
+                        }
+                        Console.WriteLine();
+                    }
+
+                    Console.SetCursorPosition(userY, userX);
+                    Console.Write('%');
+                    ConsoleKeyInfo readKey = Console.ReadKey();
+                    switch (readKey.Key)
+                    {
+                        case ConsoleKey.UpArrow:
+                            if (map[userX - 1, userY] != '#') userX--;
+                            break;
+                        case ConsoleKey.DownArrow:
+                            if (map[userX + 1, userY] != '#') userX++;
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            if (map[userX, userY - 1] != '#') userY--;
+                            break;
+                        case ConsoleKey.RightArrow:
+                            if (map[userX, userY + 1] != '#') userY++;
+                            break;
+                    }
+                    if (map[userX, userY] == 'X')
+                    {
+                        Console.SetCursorPosition(0,10);
+                        Console.WriteLine("You found the treasure!");
+                        map[userX, userY] = ' '; 
+                    }
+
+                }
+            }
+        }
+
+
+    }
+
+
+
+}
+
---
+++ console/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("console")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("console")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("8548cd5f-79b6-4701-9d4f-1f9b1b457dd7")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
